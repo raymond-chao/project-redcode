bf7aff1938d7cf94fdd1790a95a72bf95eadd151
raymond-chao <raymond_chao45@hotmail.com>
2026-05-11 16:32:20 +0200

    added fake db

---
+++ Data/Database.cs
@@ -0,0 +1,11 @@
+﻿using project_redcode.Models;
+
+namespace project_redcode.Data
+{
+    public class Database
+    {
+        public static List<Book> Books = new();
+        public static List<User> Users = new();
+        public static List<Quote> Quotes = new();
+    }
+}
