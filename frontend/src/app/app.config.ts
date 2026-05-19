b3504f7d0f9c521bf681e85eaa4abe550416aaa4
raymond-chao <raymond_chao45@hotmail.com>
2026-05-19 14:17:15 +0200

    config fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(),
+  providers: [provideExperimentalZonelessChangeDetection(),
   provideRouter(routes),
   provideHttpClient(),
   provideToastr({positionClass:'toast-top-center'})]
